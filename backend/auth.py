f2557b18a5437c95ee7b035d1bc60b5875adb0c7
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-09-08 18:09:33 -0400

    models first draft complete. User and auth routes added

--- backend/auth.py
+++ backend/auth.py
@@ -1,11 +1,18 @@
+# Package Requirements
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token
+from datetime import datetime
 import bcrypt
 
+# Local Requirements
+from models import db, User
 
+
+# Blueprint Declaration
 auth = Blueprint('auth', __name__)
 
 
+# Password Hashing & Verify Functions
 def set_password(password):
     hashed_password = bcrypt.hashpw(
         password.encode('utf-8'), bcrypt.gensalt())
@@ -18,3 +25,79 @@ def verify_password(password, hashed_password):
         return True
     else:
         return False
+
+
+# Routes
+@auth.route('/', methods=["POST"])
+def login():
+    data = request.get_json()
+    try:
+        email = data['email']
+        password = data['password']
+        if not email:
+            return jsonify(message="Email Required"), 400
+        elif not password:
+            return jsonify(message='Password Required'), 400
+
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            return jsonify(message='Email Not Valid'), 400
+
+        verified = verify_password(password, user.hashed_password)
+
+        if not verified:
+            # Error needs handling decision
+            return jsonify(message='Incorrect Password'), 403
+        else:
+            auth_token = create_access_token(
+                identity={"email": user.email}
+            )
+        return jsonify(auth_token=auth_token), 200
+
+    except Exception:
+        return jsonify(message='Login Failed'), 408
+
+
+@auth.route('/signup', methods=["POST"])
+def signup():
+    data = request.get_json()
+    try:
+        username = data['username']
+        email = data['email']
+        firstname = data['firstname']
+        lastname = data['lastname']
+        zipcode = int(data['zipcode'])
+
+        if not username:
+            return jsonify(message="Username Required"), 400
+        elif not email:
+            return jsonify(message='Email Required'), 400
+        elif not firstname:
+            return jsonify(message='First Name Required'), 400
+        elif not lastname:
+            return jsonify(message='Last Name Required'), 400
+        elif not zipcode:
+            return jsonify(message='Zipcode Required'), 400
+
+        try:
+            hashed_password = set_password(data['password'])
+        except Exception:
+            return jsonify(message='Password Required'), 400
+
+        user = User(
+            username=username,
+            email=email,
+            hashed_password=hashed_password,
+            firstname=firstname,
+            lastname=lastname,
+            zipcode=zipcode,
+            date_added=datetime.now()
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        auth_token = create_access_token(identity={"email": user.email})
+        return jsonify(auth_token=auth_token), 200
+
+    except Exception:
+        return jsonify(message="try failed"), 409
--- backend/config.py
+++ backend/config.py
@@ -6,6 +6,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
+
     # Key Variables
     SECRET_KEY = os.environ.get('SECRET_KEY')
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
