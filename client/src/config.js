04743e7c1e9341560841b4d870877e12afea6bc1
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-09-15 20:00:54 -0400

    BaseURL deploy bug

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 const BASE_URL = window.location.host
 
-export const AUTH_URL = `${BASE_URL}/auth`;
-export const API_URL = `${BASE_URL}/api`;
+export const AUTH_URL = `/auth`;
+export const API_URL = `/api`;
