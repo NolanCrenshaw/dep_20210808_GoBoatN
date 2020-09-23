18d0e8786feabcd15932ab2da7010a1a7cb8ac2e
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-09-23 12:06:51 -0400

    first attempt

--- backend/__init__.py
+++ backend/__init__.py
@@ -40,6 +40,7 @@ app.register_blueprint(bucket, url_prefix='/api/bucket')
 @app.route('/', defaults={'path': ''})
 @app.route('/<path>')
 def react_root(path):
+    print(f'Path --> {path}')
     if path == 'favicon.ico':
         return app.send_static_file('favicon.ico')
     return app.send_static_file('index.html')
