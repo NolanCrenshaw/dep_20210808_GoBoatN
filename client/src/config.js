33f68a6f6ea560f4396045057120de1bc89b79fd
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-11-12 14:42:02 -0500

    migrate system changes

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000"
+export const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3000"
 
 export const IMG_KEY = "https://goboatnbucky.s3.amazonaws.com/"
