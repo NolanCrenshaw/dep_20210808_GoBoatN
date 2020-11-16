65830d52589cc61d2e7f3d52f8a0c5e645c8fd1f
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-11-16 11:01:04 -0500

    host issues resolved

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = process.env.REACT_APP_BASE_URL || "http://127.0.0.1:5000/"
+export const BASE_URL = process.env.REACT_APP_BASE_URL || "http://172.18.32.247:5000/"
 
 export const IMG_KEY = "https://goboatnbucky.s3.amazonaws.com/"
