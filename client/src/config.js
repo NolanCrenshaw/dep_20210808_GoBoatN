2c0956416e122fbb71d9f9f4a758709b5ee578d8
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-12-06 21:27:03 -0500

    dev setup changed to pi server

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,6 @@
 // Dev BASE_URL must be set to non-static WSL2 IP.
 // flask server runs on command ``` flask run -h {IP} -p 5000 ```
-export const BASE_URL = process.env.REACT_APP_BASE_URL || "http://172.18.32.247:5000/"
+export const BASE_URL =
+  process.env.REACT_APP_BASE_URL || "http://172.17.35.99:5000";
 
-export const IMG_KEY = "https://goboatnbucky.s3.amazonaws.com/"
+export const IMG_KEY = "https://goboatnbucky.s3.amazonaws.com/";
