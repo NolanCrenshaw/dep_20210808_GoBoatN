8c1b467f1552739de6daaf6d3c6289d6dec3bb32
NolanCrenshaw <nolan.crenshaw@gmail.com>
2021-01-23 15:36:27 -0500

    added client dev .env and changed config

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,7 @@
 // Dev BASE_URL must be set to non-static WSL2 IP.
 // flask server runs on command ``` flask run -h {IP} -p 5000 ```
 export const BASE_URL =
-  process.env.REACT_APP_BASE_URL || "http://172.17.35.99:5000";
+  process.env.REACT_APP_BASE_URL ||
+  `http://${process.env.REACT_APP_DEV_URL}:5000`;
 
 export const IMG_KEY = "https://goboatnbucky.s3.amazonaws.com/";
