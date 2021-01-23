73a8379e6e79e99c184a25a43753fabcbf87bcf8
NolanCrenshaw <nolan.crenshaw@gmail.com>
2021-01-23 18:27:23 -0500

    rebuilt index and App.js functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
 // Package Requirements
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
 // Local Requirements
 import Splash from "./components/Splash";
 import Main from "./components/Main";
 
-// Access "auth_token" -- never pass to props
-const token = window.localStorage.getItem("auth_token");
-
 // React Component
 function App() {
-  // ---- Component Render ---- //
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState(null);
 
-  // -- TODO --
-  // need to validate for undefined,etc tokens
+  useEffect(() => {
+    // token sets to null when absent
+    setToken(window.localStorage.getItem("auth_token"));
+  }, []);
+
+  useEffect(() => {
+    /*
+    ~~ TODO ~~
+    Currently not secure.
+    Will need to fetch against restricted route
+    to check if token is valid.
+    */
+    if (token !== null) {
+      setIsLoggedIn(true);
+    }
+  }, [token]);
 
   // Render
-  return <BrowserRouter>{token ? <Main /> : <Splash />}</BrowserRouter>;
+  return <>{isLoggedIn ? <Main /> : <Splash />}</>;
 }
 
 export default App;
