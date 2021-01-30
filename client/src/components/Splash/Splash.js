85d8ba325dc7f9a381afb754890cc796864a23ee
NolanCrenshaw <nolan.crenshaw@gmail.com>
2021-01-29 20:10:22 -0500

    splash overhaul init complete

--- client/src/components/Splash/Splash.js
+++ client/src/components/Splash/Splash.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LoginForm from "../_forms/LoginForm";
+import SignupForm from "../_forms/SignupForm";
 
 const Splash = () => {
+  const [toggleUI, setToggleUI] = useState(false);
+  const [toggleBtn, setToggleBtn] = useState("Log In");
+
+  useEffect(() => {
+    toggleUI ? setToggleBtn("Sign Up") : setToggleBtn("Log In");
+  }, [toggleUI]);
   return (
     <div className="splash-container">
-      <LoginForm />
+      <nav>
+        <button id="toggleUI_btn" onClick={() => setToggleUI(!toggleUI)}>
+          {toggleBtn}
+        </button>
+      </nav>
+      <div className="splash_img">
+        <img
+          alt="Splash Image"
+          src="https://goboatnbucky.s3.us-east-2.amazonaws.com/josh-wedgwood-river-etive-unsplash.jpg"
+        />
+      </div>
+      <div className="splash_panel">
+        {toggleUI ? <LoginForm /> : <SignupForm />}
+      </div>
     </div>
   );
 };
---
+++ client/src/components/_forms/SignupForm.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const content = {
+  inputs: [
+    {
+      label: "Email",
+      name: "email",
+      type: "email",
+    },
+    {
+      label: "Username",
+      name: "username",
+      type: "text",
+    },
+    {
+      label: "Password",
+      name: "password",
+      type: "password",
+    },
+    // {
+    //   label: "First Name",
+    //   name: "firstname",
+    //   type: "text",
+    // },
+    // {
+    //   label: "Last Name",
+    //   name: "lastname",
+    //   type: "text",
+    // },
+  ],
+};
+
+const schema = yup.object().shape({
+  email: yup.string().required().email(),
+  username: yup.string().required().min(6),
+  password: yup.string().required().min(6),
+  // firstname: yup.string().required(),
+  // lastname: yup.string().required(),
+});
+
+const SignupForm = () => {
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const submitForm = (data) => console.log(data);
+
+  return (
+    <div className="signupform-container form">
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit(submitForm)}>
+        {content.inputs.map((input, key) => {
+          return (
+            <div className="form_element" key={key}>
+              <div>
+                <label>{input.label}</label>
+                <p>{errors[input.name]?.message}</p>
+              </div>
+              <input name={input.name} type={input.type} ref={register} />
+            </div>
+          );
+        })}
+        <button className="form_button" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SignupForm;
