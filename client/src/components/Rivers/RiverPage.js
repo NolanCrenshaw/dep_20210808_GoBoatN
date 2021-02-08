c3a12d950f99eabb4f1480cff7559d167f8f8903
NolanCrenshaw <nolan.crenshaw@gmail.com>
2021-02-08 14:47:30 -0500

    map rendering and toggling map state init

--- client/src/components/Rivers/RiverPage.js
+++ client/src/components/Rivers/RiverPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { Map, TileLayer, Marker } from "react-leaflet";
 import { motion } from "framer-motion";
 
 const RiverPage = () => {
@@ -8,6 +9,12 @@ const RiverPage = () => {
   const rivers = useSelector((state) => state.rivers);
   const [river, setRiver] = useState({ name: "undefined" });
   const [accesses, setAccesses] = useState([]);
+
+  // Map State
+  const [putin, setPutin] = useState([35.0829, -84.4918]);
+  const [takeout, setTakeout] = useState([35.0829, -84.4918]);
+  const [center, setCenter] = useState([35.0829, -84.4918]);
+  const [zoom, setZoom] = useState(11);
 
   /*
   ~~ TODO ~~
@@ -32,7 +39,17 @@ const RiverPage = () => {
     if (river) {
       setAccesses(river.accesses);
     }
+    // console.log("HELLO", accesses);
   }, [river]);
+
+  useEffect(() => {
+    if (accesses !== undefined && accesses.length !== 0) {
+      console.log("HELLO", accesses);
+      const access = accesses[0];
+      const center = [access.latitude, access.longitude];
+      setCenter(center);
+    }
+  }, [accesses]);
 
   return (
     <motion.div
@@ -44,7 +61,7 @@ const RiverPage = () => {
       <header>
         <h1>{river.name}</h1>
         <h4>{river.region}</h4>
-        <ul>
+        {/* <ul>
           {accesses ? (
             accesses.map((access) => (
               <li>
@@ -57,8 +74,34 @@ const RiverPage = () => {
           ) : (
             <div>undefined</div>
           )}
-        </ul>
+        </ul> */}
       </header>
+      <div className="map-container">
+        <Map center={center} zoom={zoom}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          {accesses ? (
+            accesses.map((access) => (
+              <Marker position={[access.latitude, access.longitude]} />
+              // <h4>{access.name}</h4>
+              // <p>{access.id}</p>
+              // <p>{access.latitude}</p>
+              // <p>{access.longitude}</p>
+            ))
+          ) : (
+            <div>undefined</div>
+          )}
+
+          <Marker position={putin}>
+            {/* <Popup>{access[0].name}</Popup> */}
+          </Marker>
+          <Marker position={takeout}>
+            {/* <Popup>{access[1].name}</Popup> */}
+          </Marker>
+        </Map>
+      </div>
     </motion.div>
   );
 };
