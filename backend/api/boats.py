ab57abf255448df661353771e2148d245c8771df
NolanCrenshaw <nolan.crenshaw@gmail.com>
2021-02-23 11:53:32 -0500

    boats route cleanup

--- backend/api/boats.py
+++ backend/api/boats.py
@@ -8,12 +8,17 @@ from ..models import db, Boat
 boat = Blueprint('boats', __name__)
 
 
-# Creates new boat row entree
-@boat.route('/', methods=["POST"])
+@boat.route('/', methods=["GET", "POST"])
 @jwt_required()
-def create_boat():
-    data = request.get_json()
-    try:
+def handle_boats():
+
+    # GET path
+    if request.method == "GET":
+        return jsonify(message="boats GET reached"), 200
+
+    # POST path
+    if request.method == "POST":
+        data = request.get_json()
         boat = Boat(
             name=data['name'],
             make=data['make'],
@@ -23,19 +28,15 @@ def create_boat():
         )
         db.session.add(boat)
         db.session.commit()
-        return jsonify(message="boat created successfully")
-    except Exception:
-        return jsonify(message="boat create failed"), 409
+        return jsonify(message="boat created successfully"), 200
 
 
-# Deletes a Boat row entree
-@boat.route('/<id>/delete', methods=["DELETE"])
+# Deletes Boat instance
+@boat.route('/delete', methods=["DELETE"])
 @jwt_required()
-def destroy_boat(id):
-    try:
-        boat = Boat.query.filter_by(id=id).first()
-        db.session.delete(boat)
-        db.session.commit()
-        return jsonify(message="boat deleted"), 200
-    except Exception:
-        return jsonify(message="destroy boat route failed"), 400
+def destroy_boat():
+    data = request.get_json()
+    boat = Boat.query.filter_by(id=data["id"]).first()
+    db.session.delete(boat)
+    db.session.commit()
+    return jsonify(message="boat deleted"), 200
