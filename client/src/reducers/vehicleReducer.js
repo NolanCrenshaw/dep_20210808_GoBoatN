67d3884e2f03df6de28159bdf8f37ed707c34ab4
NolanCrenshaw <nolan.crenshaw@gmail.com>
2021-02-23 12:29:42 -0500

    vehicle reducer buildout

--- client/src/reducers/vehicleReducer.js
+++ client/src/reducers/vehicleReducer.js
@@ -1,7 +1,24 @@
+import {
+  SET_VEHICLES_START,
+  SET_VEHICLES_SUCCESS,
+  SET_VEHICLES_FAILURE,
+} from "../actions/vehicleActions";
+
 const vehicleReducer = (state = {}, action) => {
   Object.freeze(state);
   let nextState = { ...state };
   switch (action.type) {
+    case SET_VEHICLES_START:
+      nextState.loading = true;
+      return nextState;
+    case SET_VEHICLES_SUCCESS:
+      nextState = action.vehicles;
+      nextState.loading = false;
+      return nextState;
+    case SET_VEHICLES_FAILURE:
+      nextState.error = action.error;
+      nextState.loading = false;
+      return nextState;
     default:
       return state;
   }
